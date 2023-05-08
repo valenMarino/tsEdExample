1eb12e57d226618a6fd5e9885fb4e697ee8b13a2
DESKTOP-7DS7JNG\TZ002NB <valentin.marino@santexgroup.com>
2023-05-08 10:54:57 -0300

    new features in category

--- src/controllers/rest/CategoryController.ts
+++ src/controllers/rest/CategoryController.ts
@@ -1,30 +1,59 @@
-import {Controller} from "@tsed/di";
-import {Get, Post, Delete, Put} from "@tsed/schema";
+import { Controller } from "@tsed/di";
+import { BadRequest } from "@tsed/exceptions";
+import { PathParams , BodyParams} from "@tsed/platform-params";
+import { Get, Post, Delete, Put } from "@tsed/schema";
+import { Request } from "express";
+import { Category } from "src/interfaces/category.interface";
+
+
+let categories: Category[] = [
+  { id: 1, name: 'balls' },
+  { id: 2, name: 'shoes'},
+  { id: 3, name: 'shirts'}
+]
+
+function GetResult(success:boolean, message: string) : any{
+  return { 
+    success: success,
+    message: message
+  }
+}
 
 @Controller("/category")
 export class CategoryController {
   @Get("/:id")
-  get() {
-    return {name:'book1',price:10};
+  async get(@PathParams("id") id:string) : Promise<Category> {
+    let category = categories.find(c => c.id == Number.parseInt(id)) as Category;
+    return category;
   }
 
   @Get("/")
-  getAll() {
-    return [{name:'book1',price:10},{name:'book2',price:13}];
+  async getAll() : Promise<Category[]> {
+    return categories;
   }
 
   @Post("/")
-  post() {
-    return 'create sucessfully';
+  async post(@BodyParams() body: any) : Promise<any> {
+    let category : Category = {
+      id: body.id,
+      name: body.name
+    }
+    categories.push(category)
+    return GetResult(true, 'saved succesfully');
   }
 
   @Put("/:id")
-  update() {
-    return 'update sucessfully';
+  async update(@PathParams("id") id:string, @BodyParams() body: any) : Promise<any>{
+    let category = categories.find(c => c.id == Number.parseInt(id)) as Category;
+    if(category){
+      category.name = body.name;
+      return GetResult(true, 'updated succesfully');
+    }
+    return BadRequest;
   }
 
   @Delete("/:id")
-  delete() {
-    return 'delete sucessfully';
+  async delete(@PathParams("id") id:string) : Promise<any>{
+    return GetResult(true, 'updated succesfully');
   }
 }
